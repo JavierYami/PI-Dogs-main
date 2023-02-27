322f0fed8c94ec302eb671dcd67a13a1c29cf673
JavierYami <javieyami1@outlook.com>
2023-02-27 11:43:34 -0600

    Prototipo de rutas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,5 +8,24 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/dogs', (req, res)=>{
+    res.status(200).send('Hola')
+})
+
+router.get('/dogs/:idRaza', (req, res)=>{
+    res.status(200).send('Hola')
+})
+
+router.post('/dogs', (req, res) =>{
+    res.status(200).send('Hola')
+})
+
+router.get('/temperaments', (req, res)=>{
+    res.status(200).send('Hola')
+})
+
+
+
+
 
 module.exports = router;
