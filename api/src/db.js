db7b6b1689d4eec39b598a30a728e366cdb88257
JavierYami <javieyami1@outlook.com>
2023-02-27 19:10:55 -0600

    Prototipo de modelos

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,9 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dogs, Temperaments } = sequelize.models;
+
+console.log(sequelize.models)
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
